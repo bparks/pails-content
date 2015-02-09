038f2faa431e3d5387eeb0e78f7bd7477c931197
Brian Parks <bparks@synapsesoftware.com>
2015-02-09 11:16:43 -0700

    might help if we use ckeditor on the 'new' page, too

--- views/node/new.php
+++ views/node/new.php
@@ -1,6 +1,12 @@
+<script src="/lib/content/ckeditor/ckeditor.js"></script>
+
 <form action="/node/create" method="POST">
 <?php echo $this->input_for('title', 'Title'); ?>
 <?php echo $this->input_for('slug', 'Slug'); ?>
-<?php echo $this->input_for('body', 'Content', array('type' => 'textarea')); ?>
+<?php echo $this->input_for('body', 'Content', array('type' => 'textarea', 'class' => 'ckeditor')); ?>
 <input type="submit" value="Save" />
 </form>
+
+<script>
+CKEDITOR.replace('.ckeditor');
+</script>
