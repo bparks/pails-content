cf72a3ddfc82057425ae8bb7f04f1c9a00802454
Brian Parks <bparks@synapsesoftware.com>
2015-07-06 17:44:51 -0600

    Object-oriented initializer

--- index.php
+++ index.php
@@ -5,22 +5,25 @@ require_once(__DIR__.'/models/NodeBody.php');
 
 //Controllers and views get included automatically
 
-function content_config($app)
+class PailsContent
 {
-	$app->registerRouter(function ($uri) {
-		$req = new Pails\Request();
-		if (Node::find_by_slug($uri))
-		{
-			$req->controller = 'node';
-			$req->action = $uri;
-			$req->raw_parts = array($req->controller, $req->action);
-			return $req;
-		}
-		return false;
-	});
-
-	if (defined('ADMIN_MENU_SLUG'))
+	static function initialize($app)
 	{
-		Menu::add_static_item(ADMIN_MENU_SLUG, 'Content', '/node');
+		$app->registerRouter(function ($uri) {
+			$req = new Pails\Request();
+			if (Node::find_by_slug($uri))
+			{
+				$req->controller = 'node';
+				$req->action = $uri;
+				$req->raw_parts = array($req->controller, $req->action);
+				return $req;
+			}
+			return false;
+		});
+
+		if (defined('ADMIN_MENU_SLUG'))
+		{
+			Menu::add_static_item(ADMIN_MENU_SLUG, 'Content', '/node');
+		}
 	}
 }
